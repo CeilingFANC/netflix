1ccf5d6dadd45719bc8cac2065aca7b05d32ac9c
CeilingFANC <cf10293@163.com>
2019-08-14 13:02:03 -0700

    feat: Improved user experience

--- src/Component/movieIcon.js
+++ src/Component/movieIcon.js
@@ -5,6 +5,7 @@ function MovieIcon(props){
     const {title,img} = props.movie;
     
     return <div className="contain">
+        <h4 className="icon-title">{title}</h4>
         <img src={img} alt={title}></img>
         <div className="overlay">
             <div className="text" onClick={props.clickHandler}>{props.textButton}</div>
